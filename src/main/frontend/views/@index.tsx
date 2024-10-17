b682909bb6636201215bdf0b6e66bde3c1f6ce3f
Sophia Peckner <sophiapeckner@gmail.com>
2024-10-17 17:03:58 +0200

    feat: initial commit

--- src/main/frontend/views/@index.tsx
+++ src/main/frontend/views/@index.tsx
@@ -5,9 +5,42 @@ export const config: ViewConfig = { menu: { order: 0, icon: 'line-awesome/svg/fi
 export default function LogInView() {
   return (
     <div className="flex flex-col h-full items-center justify-center p-l text-center box-border">
-      <img style={{ width: '200px' }} src="images/empty-plant.png" />
-      <h2>This place intentionally left empty</h2>
-      <p>It’s a place where you can grow your own UI 🤗</p>
+        <form className="login-form">
+            <img style={{ width: '100%' }} src="images/movie_reel.png" />
+            <div className="input">
+                <label>Email</label>
+                <br />
+                <input
+                  type="text"
+                  id="email"
+                  name="email"
+                  placeholder="example@gmail.com"
+                />
+            </div>
+
+            <div className="input">
+                <label>Password</label>
+                <br />
+                <input
+                  type="password"
+                  id="password"
+                  name="password"
+                  placeholder="**********"
+                />
+            </div>
+
+            <a href="/">
+                <input className="sign-in" value="Sign In" />
+            </a>
+
+            <div className="redirect">
+                <a href="/forgotPassword">Forgot Password</a>
+                <a href="/sign-up">Sign Up</a>
+            </div>
+        </form>
+        <a href="/join-group">
+          <button className="guest-btn">Continue as Guest</button>
+        </a>
     </div>
   );
 }
