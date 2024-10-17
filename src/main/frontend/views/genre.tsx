0303842e363ee3923eb25be0e5e5ff6488649a90
Sophia Peckner <sophiapeckner@gmail.com>
2024-10-17 18:09:37 +0200

    feat: add Pick Genre View

--- src/main/frontend/views/genre.tsx
+++ src/main/frontend/views/genre.tsx
@@ -5,9 +5,26 @@ export const config: ViewConfig = { menu: { order: 5, icon: 'line-awesome/svg/fi
 export default function PickGenreView() {
   return (
     <div className="flex flex-col h-full items-center justify-center p-l text-center box-border">
-      <img style={{ width: '200px' }} src="images/empty-plant.png" />
-      <h2>This place intentionally left empty</h2>
-      <p>It’s a place where you can grow your own UI 🤗</p>
+      <h2>flickr</h2>
+      <form className="restrict-form">
+        <label htmlFor="Genre">Genre: </label>
+        <select name="comedy" id="comedy">
+          <option>Comedy</option>
+          <option>Action</option>
+        </select>
+        <br />
+        <br />
+        <label htmlFor="Rating">Rating: </label>
+        <select name="R" id="R">
+          <option>R</option>
+          <option>PG-13</option>
+        </select>
+        <br />
+        <br />
+        <a href="/members">
+          <input className="ready-btn" value="Ready" />
+        </a>
+      </form>
     </div>
   );
 }
