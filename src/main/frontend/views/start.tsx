e5958a4bf589798f5fbe41d8796484180c87ad24
Sophia Peckner <sophiapeckner@gmail.com>
2024-10-17 17:36:51 +0200

    feat: add start view

--- src/main/frontend/views/start.tsx
+++ src/main/frontend/views/start.tsx
@@ -5,9 +5,18 @@ export const config: ViewConfig = { menu: { order: 2, icon: 'line-awesome/svg/fi
 export default function StartView() {
   return (
     <div className="flex flex-col h-full items-center justify-center p-l text-center box-border">
-      <img style={{ width: '200px' }} src="images/empty-plant.png" />
-      <h2>This place intentionally left empty</h2>
-      <p>It’s a place where you can grow your own UI 🤗</p>
+      <a className="login-link" href="/login">
+        Login
+      </a>
+      <div className="home-page">
+        <h2 className="page-title">flickr</h2>
+        <a href="/join-group">
+          <button className="join-group-btn">Join Group</button>
+        </a>
+        <p className="sign-in-prompt">
+          Please <a href="/login" style={{ textDecoration: 'underline' }}>login</a> or <a href="/sign-up" style={{ textDecoration: 'underline' }}>sign up</a> to create a group
+        </p>
+      </div>
     </div>
   );
 }
