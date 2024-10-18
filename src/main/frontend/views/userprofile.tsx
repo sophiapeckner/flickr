0a810f3203116f295898fd9d55e9f035f300be3e
Sophia Peckner <sophiapeckner@gmail.com>
2024-10-18 11:48:22 +0200

    feat add User Profile

--- src/main/frontend/views/userprofile.tsx
+++ src/main/frontend/views/userprofile.tsx
@@ -5,9 +5,53 @@ export const config: ViewConfig = { menu: { order: 8, icon: 'line-awesome/svg/fi
 export default function UserProfileView() {
   return (
     <div className="flex flex-col h-full items-center justify-center p-l text-center box-border">
-      <img style={{ width: '200px' }} src="images/empty-plant.png" />
-      <h2>This place intentionally left empty</h2>
-      <p>It’s a place where you can grow your own UI 🤗</p>
+      <a href="/signed-in-start-page">
+        <button className="back-button">
+          X
+        </button>
+      </a>
+      <h2>flickr</h2>
+      <h3 className="username">ExampleUser Profile</h3>
+      <form className="profile-form">
+        <div className="input">
+          <label className="emailLabel">
+            Username
+          </label>
+          <br />
+          <input
+            type="text"
+            id="email"
+            name="email"
+            className="emailInput"
+            placeholder="example-username"
+          />
+        </div>
+
+        <div className="input">
+          <label>Password</label>
+          <br />
+          <input
+            type="password"
+            id="password"
+            name="password"
+            placeholder="**********"
+          />
+        </div>
+
+        <div className="input">
+          <label className="emailLabel">
+            Email
+          </label>
+          <br />
+          <input
+            type="text"
+            id="email"
+            name="email"
+            className="emailInput"
+            placeholder="example@gmail.com"
+          />
+        </div>
+      </form>
     </div>
   );
 }
