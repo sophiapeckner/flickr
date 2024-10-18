c483b2640a24ba6d08315762f1a8d1e772edbf86
Ryan Schmitt <ryan15schmitt@gmail.com>
2024-10-18 14:24:57 +0200

    Improved bottom nav bar styles

--- src/main/frontend/views/@index.tsx
+++ src/main/frontend/views/@index.tsx
@@ -1,46 +1,49 @@
-import { ViewConfig } from '@vaadin/hilla-file-router/types.js';
+import { ViewConfig } from "@vaadin/hilla-file-router/types.js";
 
-export const config: ViewConfig = { menu: { order: 0, icon: 'line-awesome/svg/file.svg' }, title: 'Log In' };
+export const config: ViewConfig = {
+  menu: { order: 0, icon: "line-awesome/svg/file.svg" },
+  title: "Log In",
+};
 
 export default function LogInView() {
   return (
     <div className="flex flex-col h-full items-center justify-center p-l text-center box-border">
-        <img style={{ width: '100%' }} src="images/movie_reel.png" />
-        <form className="login-form">
-            <div className="input">
-                <label>Email</label>
-                <br />
-                <input
-                  type="text"
-                  id="email"
-                  name="email"
-                  placeholder="example@gmail.com"
-                />
-            </div>
+      <img style={{ width: "100%" }} src="images/movie_reel.png" />
+      <form className="login-form">
+        <div className="input">
+          <label>Email</label>
+          <br />
+          <input
+            type="text"
+            id="email"
+            name="email"
+            placeholder="example@gmail.com"
+          />
+        </div>
 
-            <div className="input">
-                <label>Password</label>
-                <br />
-                <input
-                  type="password"
-                  id="password"
-                  name="password"
-                  placeholder="**********"
-                />
-            </div>
+        <div className="input">
+          <label>Password</label>
+          <br />
+          <input
+            type="password"
+            id="password"
+            name="password"
+            placeholder="**********"
+          />
+        </div>
 
-            <a href="/">
-                <input className="sign-in" value="Sign In" />
-            </a>
-
-            <div className="redirect">
-                <a href="/forgotPassword">Forgot Password</a>
-                <a href="/signup">Sign Up</a>
-            </div>
-        </form>
-        <a href="/join-group">
-          <button className="guest-btn">Continue as Guest</button>
+        <a href="/">
+          <input className="sign-in" value="Sign In" />
         </a>
+
+        <div className="redirect">
+          <a href="/forgotPassword">Forgot Password</a>
+          <a href="/signup">Sign Up</a>
+        </div>
+      </form>
+      <a href="/join-group">
+        <button className="guest-btn">Continue as Guest</button>
+      </a>
     </div>
   );
 }
