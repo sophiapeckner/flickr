0459aa5f2a4e4269112bfa9b23c084bfec3d9fbb
Sophia Peckner <sophiapeckner@gmail.com>
2024-10-18 11:07:52 +0200

    feat: create Group Landing page

--- src/main/frontend/views/grouplanding.tsx
+++ src/main/frontend/views/grouplanding.tsx
@@ -5,9 +5,38 @@ export const config: ViewConfig = { menu: { order: 4, icon: 'line-awesome/svg/fi
 export default function GroupLandingView() {
   return (
     <div className="flex flex-col h-full items-center justify-center p-l text-center box-border">
-      <img style={{ width: '200px' }} src="images/empty-plant.png" />
-      <h2>This place intentionally left empty</h2>
-      <p>It’s a place where you can grow your own UI 🤗</p>
+      <div>
+        <a href="/">
+          <button className="back-button">
+            X
+          </button>
+        </a>
+        <a href="/userprofile">
+          <img className="profile-img" src="images/profile.png" />
+        </a>
+      </div>
+      <h2>flickr</h2>
+      <h6 className="group-title">Group Code: </h6>
+      <h3 className="group-code">XPJMRT</h3>
+
+      <div className="members">
+        <div className="person">
+          <img className="person-img" src="images/person.png" alt="" />
+          <h4>Jane Mustang</h4>
+        </div>
+        <div className="person">
+          <img className="person-img" src="images/person.png" alt="" />
+          <h4>Jane Doe</h4>
+        </div>
+      </div>
+
+      <div className="session-div">
+        <button className="start-session-btn">
+          <a className="start-session-link" href="/movie-selection">
+            Start Session
+          </a>
+        </button>
+      </div>
     </div>
   );
 }
