de5d3dd14783005acfcc23d6faa15f54c5897bbc
Sydney <sydlyn626@gmail.com>
2024-11-20 11:03:57 +0100

    start page unauth log in prompt

--- src/main/frontend/views/start.tsx
+++ src/main/frontend/views/start.tsx
@@ -1,12 +1,13 @@
 import { ViewConfig } from "@vaadin/hilla-file-router/types.js";
-import {useEffect, useState} from "react";
-import Session from "Frontend/generated/com/flickr/entities/Session";
-import {createSession, findAll, joinSession} from "Frontend/generated/SessionEndpoint";
 import { style } from "../themes/flickr/css.js";
+import {colors} from "Frontend/themes/flickr/colors";
+import {useEffect, useState} from "react";
+import {createSession, joinSession} from "Frontend/generated/SessionEndpoint";
+
 import {getMember, isLoggedIn} from "Frontend/auth";
-import Member from "Frontend/generated/com/flickr/entities/Member";
 import {Button} from "@vaadin/react-components";
 import {CustomHeader} from "Frontend/themes/flickr/elements";
+
 
 export const config: ViewConfig = {
   menu: { order: 2, icon: "line-awesome/svg/file.svg" },
@@ -67,8 +68,20 @@ export default function StartView() {
         <h2 style={style.pageTitle}>flickr</h2>
         <div style={{...style.innerDiv, ...style.innerDivAddOn}}>
           <Button style={style.groupChoiceButton} onClick={handleJoinGroup}>Join Group</Button>
-          {allowCreateGroup && (
+          {allowCreateGroup ? (
               <Button style={style.groupChoiceButton} onClick={handleCreateGroup}>Create Group</Button>
+          ) : (
+            <p style={{color: colors.main}}>
+              Please{" "}
+              <a href="/" style={{textDecoration: "underline"}}>
+                Log In
+              </a>{" "}
+              or{" "}
+              <a href="/signup" style={{textDecoration: "underline"}}>
+                Sign Up
+              </a>{" "}
+              to create a group.
+            </p>
           )}
         </div>
       </div>
