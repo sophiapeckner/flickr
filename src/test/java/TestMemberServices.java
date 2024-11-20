0cbab1da965a1ac2a375b3e729e42b14a65c016c
MStavr <mtstavros12@gmail.com>
2024-11-20 15:06:56 +0100

    One test out of 32! WOW! This is massive. If only there weren't associated warnings

--- src/test/java/TestMemberServices.java
+++ src/test/java/TestMemberServices.java
@@ -1,13 +1,40 @@
 import com.flickr.endpoints.MemberServices;
 import com.flickr.entities.Member;
+import com.flickr.storage.MemberRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class TestMemberServices {
-//    private MemberServices memberServicesTestObj = new MemberServices();
-    @Test
-    public void testCreateMember(){
 
+    @Mock
+    private MemberRepository mockMemberRepository;
+
+    @InjectMocks
+    private MemberServices memberServicesTestObj = new MemberServices(mockMemberRepository);
+
+    @BeforeEach
+    public void setup() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    public void testLoginAbsent(){
+        String logEmail = "thisemail@gmail.net";
+        String logPassword = "thisPass";
+        final String expected = "User Not found";
+        Mockito.when(mockMemberRepository.findByEmail(logEmail)).thenReturn(Optional.empty());
+
+        Assertions.assertEquals(expected, memberServicesTestObj.login(logEmail, logPassword));
+        Mockito.verify(mockMemberRepository, Mockito.times(1)).findByEmail(logEmail);
     }
 }
